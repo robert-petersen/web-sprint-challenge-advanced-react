f97f1382919503980adaab0a66c71fe238166f23
Robert Petersen <robert.petersen808@gmail.com>
2020-12-04 11:55:15 -0600

    tests passing

--- client/src/components/CheckoutForm.test.js
+++ client/src/components/CheckoutForm.test.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { render, screen, userEvent  } from "@testing-library/react";
+import { render, screen, fireEvent } from "@testing-library/react";
 import CheckoutForm from "./CheckoutForm";
 
 // Write up the two tests here and make sure they are testing what the title shows
@@ -23,16 +23,23 @@ test("form shows success message on submit with form details", () => {
   const state = screen.getByLabelText(/state/i);
   const zip = screen.getByLabelText(/zip/i);
 
-  userEvent.type(firstName, "robert");
-  userEvent.type(lastName, "rob");
-  userEvent.type(address, "rob");
-  userEvent.type(city, "rob");
-  userEvent.type(state, "rob");
-  userEvent.type(zip, "rob");
+  expect(firstName).toBeInTheDocument();
+  expect(lastName).toBeInTheDocument();
+  expect(address).toBeInTheDocument();
+  expect(city).toBeInTheDocument();
+  expect(state).toBeInTheDocument();
+  expect(zip).toBeInTheDocument();
+
+  fireEvent.change(firstName, { target: { value: 'rob' } });
+  fireEvent.change(lastName, { target: { value: 'rob' } });
+  fireEvent.change(address, { target: { value: 'rob' } });
+  fireEvent.change(city, { target: { value: 'rob' } });
+  fireEvent.change(state, { target: { value: 'rob' } });
+  fireEvent.change(zip, { target: { value: 'rob' } });
 
   const checkOut = screen.getByRole("button");
-  userEvent.click(checkOut);
+  fireEvent.click(checkOut);
 
-  const successMessage = screen.queryByText("robert");
+  const successMessage = screen.queryByText("rob");
   expect(successMessage).toBeInTheDocument();
 });
